2ceb2de589e06c39ded05925b425c68fd565fa54
leroi <leroidejesa@gmail.com>
2015-07-02 12:56:02 -0700

    complete basic CRUD functionality for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,7 +10,7 @@ class ProjectsController < ApplicationController
 
   def create
     @skill = Skill.find(params[:skill_id])
-    @project = @skill.projects.new(project_params)
+    @project = @skill.projects.new(projects_params)
     if @project.save
       redirect_to skill_path(@project.skill)
     else
@@ -18,8 +18,27 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+    @skill = Skill.find(params[:skill_id])
+    @project = @skill.projects.find(params[:id])
+  end
+
+  def update
+    @skill = Skill.find(params[:skill_id])
+    @project = @skill.projects.find(params[:id])
+    @project.update(projects_params)
+    redirect_to skill_path(@skill)
+  end
+
+  def destroy
+    @skill = Skill.find(params[:skill_id])
+    @project = @skill.projects.find(params[:id])
+    @project.destroy
+    redirect_to skill_path(@skill)
+  end
+
 private
-  def project_params
+  def projects_params
     params.require(:project).permit(:description, :name, :link)
   end
 end
