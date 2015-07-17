6369a5d98cef6a8565bbba3e9399c1c9e15f6917
leroi <leroidejesa@gmail.com>
2015-07-17 11:40:32 -0700

    implement ajax - comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  
   def show
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
@@ -6,19 +8,36 @@ class CommentsController < ApplicationController
 
   def new
     @post = Post.find(params[:post_id])
+    @comment = @post.comments.new
   end
 
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comments_params)
     if @comment.save
-      redirect_to post_path(@post)
+      flash[:notice] = 'Comment was successfully added.'
+      respond_to do |format|
+        format.html { redirect_to :back  }
+        format.js
+      end
     else
       render :new
     end
   end
 
+  def update
+    if @comment.update(comment_params)
+      redirect_to @comment, notice: 'Comment was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
 private
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
   def comments_params
     params.require(:comment).permit(:text).merge :user_id => current_user.id
   end
